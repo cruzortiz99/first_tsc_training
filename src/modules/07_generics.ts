a0dfe0f2b467d608ca067a3f0b05c184e7218002
Cruz Ortiz <cortiz99>
2019-04-04 15:25:22 -0400

    generics

---
+++ src/modules/07_generics.ts
@@ -0,0 +1,28 @@
+// Given a function
+function echo(data: any) {
+  return data
+}
+// Type check for any, is not safe
+console.log(echo("Cruz"))
+console.log(echo(2))
+console.log(echo({ name: "Cruz", age: 27 }))
+
+// Using generics
+function betterEcho<T>(data: T) {
+  return data
+}
+// Correct type check 
+console.log(betterEcho<string>("Cruz"))
+console.log(betterEcho<number>(2))
+// console.log(betterEcho<number>('2')) /* Type check error */
+console.log(betterEcho({ name: "Cruz", age: 27 }))
+
+// Arrays
+const arrayNumber: Array<number> = [25, 36]
+arrayNumber.push(1)
+// arrayNumber.push('sdasda') /* Type check error */
+
+// Using generic classes extending others types 
+class SomeGeneric <T extends number|string > {
+
+}
