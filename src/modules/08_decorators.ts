5fca8c2770a86f8fff43fad521c1cfd9ed2f22c0
Cruz Ortiz <cortiz99>
2019-04-05 18:55:50 -0400

    done decorators

--- src/modules/08_decorators.ts
+++ src/modules/08_decorators.ts
@@ -16,9 +16,40 @@ class DecoPerson {
 
 // Factory
 function loggin(value: boolean) {
-  return value ? logged : () =>{}
+  return value ? logged : () => { }
 }
 @loggin(true)
 class DecoCar {
 
+}
+
+// Advanced
+function printable(constructorFn: Function) {
+  constructorFn.prototype.print = function () {
+    console.log(this)
+  }
+}
+@printable
+class OtherPlant {
+  name = "Green Plant"
+}
+const newPlant = new OtherPlant()
+// newPlant.print() /* Error because strict mode is on */
+
+// Method decoratror
+
+function editable(value: boolean) {
+  return function (target: any, property: string, descriptor: PropertyDescriptor) {
+    descriptor.writable = false
+  }
+}
+class MethodDecated {
+  @editable(false)
+  calcBuget() {
+    console.log(100)
+  }
+}
+const decorated = new MethodDecated()
+decorated.calcBuget = function () {
+  console.log(10)
 }
