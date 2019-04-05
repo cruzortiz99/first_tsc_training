e1b7f0fdb0c24e50666e50cabc85a0dac0ee9c94
Cruz Ortiz <cortiz99>
2019-04-05 06:18:43 -0400

    decorators

---
+++ src/modules/08_decorators.ts
@@ -0,0 +1,24 @@
+// ------------------------------
+// Decorators
+// ------------------------------
+
+// Class level decorator, TS will give to these types of decorators only the constructors 
+function logged(constructorFn: Function) {
+  console.log(constructorFn)
+}
+// attaching decorators to a function
+@logged
+class DecoPerson {
+  constructor() {
+    console.log('hi')
+  }
+}
+
+// Factory
+function loggin(value: boolean) {
+  return value ? logged : null
+}
+@loggin(true)
+class DecoCar {
+
+}
